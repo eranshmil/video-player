ecc794554d64398f16f650e7d4982c41eb968a55
eranshmil <eranshmil@users.noreply.github.com>
2018-11-19 15:26:59 +0200

    chore(project): rename ui project

--- projects/video-player-ui/src/app/app.component.spec.ts
+++ projects/video-player-ui/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'renderer'`, () => {
+  it(`should have as title 'video-player-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('renderer');
+    expect(app.title).toEqual('video-player-ui');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to renderer!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to video-player-ui!');
   });
 });
