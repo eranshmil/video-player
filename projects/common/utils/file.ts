6fd4ee70769345a42a094e9be63b3168705a9f0a
eranshmil <eranshmil@users.noreply.github.com>
2018-11-29 14:39:13 +0200

    fix(open): show vtt files in open dialog

--- projects/common/utils/file.ts
+++ projects/common/utils/file.ts
@@ -1,6 +1,6 @@
 import * as url from 'url';
 
-export const ALLOWED_EXTENSIONS = ['mp4', 'm4v', 'ogv', 'webm'];
+export const ALLOWED_EXTENSIONS = ['mp4', 'm4v', 'ogv', 'webm', 'vtt'];
 
 export function createLocalUrl(pathname: string): string {
   return url.format({
